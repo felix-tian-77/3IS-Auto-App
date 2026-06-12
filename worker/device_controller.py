ee7a6624c9d31404de6c79f212006a1b3d576c25
felixtian <felix_tian_gz@126.com>
2026-06-12 14:28:43 +0800

    commit

--- worker/device_controller.py
+++ worker/device_controller.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 from airtest.core.android.adb import ADB
+from airtest.core.android.android import Android
 
 class DeviceController:
     def __init__(self, adb_serial: str):
@@ -11,8 +12,8 @@ class DeviceController:
     def connect(self) -> bool:
         """Connect to device via ADB"""
         try:
-            self.adb.connect_device([self.adb_serial])
-            self.device = self.adb.device()
+            self.device = Android(self.adb_serial)
+            self.adb.connect()
             return True
         except Exception as e:
             print(f"ADB connection failed: {e}")
@@ -20,20 +21,20 @@ class DeviceController:
 
     def disconnect(self):
         """Disconnect from device"""
-        if self.adb:
-            self.adb.disconnect(self.adb_serial)
+        if self.device:
+            self.device.disconnect()
 
     def shell(self, cmd: str) -> str:
         """Execute shell command on device"""
-        return self.adb.shell(self.adb_serial, cmd)
+        return self.device.shell(cmd)
 
     def push_file(self, local_path: str, remote_path: str):
         """Push file to device"""
-        self.adb.push(self.adb_serial, local_path, remote_path)
+        self.device.push(local_path, remote_path)
 
     def pull_file(self, remote_path: str, local_path: str):
         """Pull file from device"""
-        self.adb.pull(self.adb_serial, remote_path, local_path)
+        self.device.pull(remote_path, local_path)
 
     def get_device_info(self) -> dict:
         """Get device info"""
